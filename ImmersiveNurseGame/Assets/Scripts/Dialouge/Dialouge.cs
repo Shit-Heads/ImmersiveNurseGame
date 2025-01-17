661ac6a01383c960377af5b8f535c59c0d362453
Aravinth SS <aravinthssivakumar@outlook.com>
2025-01-17 20:28:05 +0530

    dialouge system completed

--- ImmersiveNurseGame/Assets/Scripts/Dialouge/Dialouge.cs
+++ ImmersiveNurseGame/Assets/Scripts/Dialouge/Dialouge.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dialouge : MonoBehaviour
+[System.Serializable]
+public class Dialouge
 {
     public string name;
 

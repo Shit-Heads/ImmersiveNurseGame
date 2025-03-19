be34892ff833e3d2d704ab99dbe864f20cfa6db0
Aravinth SS <aravinthssivakumar@outlook.com>
2025-03-19 14:39:46 +0530

    async

--- ImmersiveNurseGame/Assets/Scripts/LevelLoader/AsyncLoader/Loader.cs
+++ ImmersiveNurseGame/Assets/Scripts/LevelLoader/AsyncLoader/Loader.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Loader : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadSceneAsync(4);
     }
 
     // Update is called once per frame
